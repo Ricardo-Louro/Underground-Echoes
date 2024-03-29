9bd79a6deee69c3c988ffe82a0e874144971df08
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-03-29 19:55:54 +0000

    Add train controls end interactible (incomplete). Remove entrance light.

---
+++ Assets/Script/TrainControls.cs
@@ -0,0 +1,12 @@
+using JetBrains.Rider.Unity.Editor;
+using UnityEngine;
+
+public class TrainControls : Interactible
+{
+    public override string interactionMessage { get; set; } = "Ride the train";
+
+    public override void Interact()
+    {
+        //END GAME
+    }
+}
