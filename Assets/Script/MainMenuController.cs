9fec8b1db37dde328cb22d84fc8a19c4f67494bf
Ricardo Louro <ricardo.f.louro@gmail.com>
2024-04-02 20:53:04 +0100

    Fixed issue where the cursor would not be visible and unlocked when returning to the main menu after completing the game.

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
     public void SwitchSceneToGame()
     {
         SceneManager.LoadScene("Gameplay");
